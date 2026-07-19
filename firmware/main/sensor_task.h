3c9f07419a0ae4e34c1958f7b193dc42d02523ad
Zachary Levesque <zlevesque93@gmail.com>
2026-07-19 18:59:36 -0400

    Update sensor_task.h

--- firmware/main/sensor_task.h
+++ firmware/main/sensor_task.h
@@ -3,6 +3,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
-// Starts the sensor polling task. Events are pushed to event_queue
-// whenever the water state changes (debounced).
-void sensor_task_start(QueueHandle_t event_queue);
+// Starts the sensor polling task. Debounced state changes are pushed to
+// alarm_event_queue first, then optionally to alert_event_queue.
+void sensor_task_start(QueueHandle_t alarm_event_queue, QueueHandle_t alert_event_queue);
